684fee58b434799629f8f19091cbbc19252f4351
bblu <gmwblu@gmail.com>
2016-04-02 00:28:43 +0800

    add circletext class

--- mousewheel/mousewheel.py
+++ mousewheel/mousewheel.py
@@ -21,6 +21,8 @@ class Vec2d(object):
     def __sub__(self, other):
         if isinstance(other, Vec2d):
             return Vec2d(self.x - other.x, self.y - other.y)
+        else:
+            return Vec2d(self.x - other, self.y - other)
 #rotate
     def rotate(self, angle_degrees):
         radians = math.radians(angle_degrees)
@@ -28,11 +30,16 @@ class Vec2d(object):
         sin = math.sin(radians)
         x = self.x*cos - self.y*sin
         y = self.x*sin + self.y*cos
+        #print ('org %d=(%d,%d)')%(angle_degrees,x,y)
         self.x = x
         self.y = y
 
 # set up the window
-screen = pygame.display.set_mode((400, 300), 0, 32)
+
+srcW = 640
+srcH = 480
+cenPos = Vec2d(srcW/2,srcH/2)
+screen = pygame.display.set_mode((640, 480), 0, 32)
 pygame.display.set_caption('Drawing')
 
 # set up the colors
@@ -47,31 +54,47 @@ UP = 'up'
 DOWN = 'down'
 
 abcIndex=0
-abcString = 'ABCDEFGHIGKLMNOPQRSTUVWXYXZ'
-curString = '?'
-curFont = pygame.font.SysFont('freemono',128)
+abcStr = 'ABCDEFGHIGKLMNOPQRSTUVWXYXZ'
+pygame.init()
+strFont = pygame.font.get_default_font();
+curFont = pygame.font.SysFont(strFont,128)
     
 class CircleText:
-    pos = Vec2d(0,0)
-    rad = 10
+    pos = Vec2d(0.0,0.0)
+    rad = 10.0
     text= ''
-    font=pygame.font.SysFont('freemono',128)
+    dAngle=0.0
+    font=pygame.font.SysFont('freemono',32)
     def __init__(self,text,font):
         self.text = text
         self.font = font
-    def setPos(ref):
-        pos = ref
-    def render(sur,idx):
+        self.dAngle = 360.0/len(text)
+        print ('%f')%self.dAngle
         
-        
+    def setPos(self, p):
+        self.pos = p
+    def setRad(self, r):
+        self.rad = r
+    def render(self, sur, idx):
+        start = -1*(self.dAngle*idx)
+        vr = Vec2d(0,-self.rad)
+        #print ('index=%d a=%f start = %d')%(idx,self.dAngle,start)
+        vr.rotate(start)
+        rot = start
+        for abc in abcStr:
+            lbl = self.font.render(abc,1,BLUE)
+            obj = pygame.transform.rotate(lbl,rot)
+            cur = vr+self.pos
+            sur.blit(obj,(cur.x,cur.y))
+            vr.rotate(self.dAngle)
+            rot -= self.dAngle
 
 def showChar(surface,index):
     global curFont,abcString,curString
     index = index%25
-    curString = abcString[index-3:index+2]
+    curString = abcStr[index]
     textobj = curFont.render(curString,1,BLUE)
-    objTest = pygame.transform.rotate(textobj,index)
-    surface.blit(objTest,(0,0))
+    surface.blit(textobj,(cenPos.x,cenPos.y))
     
 def rollWheel(button):
     global abcIndex
@@ -80,7 +103,9 @@ def rollWheel(button):
     elif button == 5 :
         abcIndex-=1
 
-pygame.init()
+abcCir = CircleText(abcStr,curFont)
+abcCir.setPos(cenPos-20)
+abcCir.setRad(cenPos.y)
 # run the game loop
 while True:
     for event in pygame.event.get():
@@ -90,5 +115,6 @@ while True:
         elif event.type == MOUSEBUTTONDOWN:
             rollWheel(event.button)
             screen.fill(GRAY)
+            abcCir.render(screen,abcIndex)
             showChar(screen,abcIndex)
     pygame.display.update()
